d4b61c12eb72237c61c5b4821fbbd9d583463c71
Parul Chauhan <parulchauhan1357@gmail.com>
2025-01-05 10:27:49 +0530

    Ques: 3 Sum

---
+++ Array/Hard/3Sum.cpp
@@ -0,0 +1,79 @@
+// 3 Sum : Find triplets that add up to a zero
+// Problem Statement: Given an array of N integers, your task is to find unique triplets that add up to give a sum of zero. In short, you need to return an array of all the unique triplets [arr[a], arr[b], arr[c]] such that i!=j, j!=k, k!=i, and their sum is equal to zero.
+// Pre-requisite: 2 Sum Problem
+
+// Examples
+// Example 1:
+// Input:
+//  nums = [-1,0,1,2,-1,-4]
+// Output:
+//  [[-1,-1,2],[-1,0,1]]
+// Explanation:
+//  Out of all possible unique triplets possible, [-1,-1,2] and [-1,0,1] satisfy the condition of summing up to zero with i!=j!=k
+
+// Example 2:
+// Input:
+//  nums=[-1,0,1,0]
+// Output:
+//  Output: [[-1,0,1],[-1,1,0]]
+// Explanation:
+//  Out of all possible unique triplets possible, [-1,0,1] and [-1,1,0] satisfy the condition of summing up to zero with i!=j!=k
+
+
+//Optimal Approach
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> triplet(int n, vector<int> &arr) {
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; i++) {
+        //remove duplicates:
+        if (i != 0 && arr[i] == arr[i - 1]) continue;
+
+        //moving 2 pointers:
+        int j = i + 1;
+        int k = n - 1;
+        while (j < k) {
+            int sum = arr[i] + arr[j] + arr[k];
+            if (sum < 0) {
+                j++;
+            }
+            else if (sum > 0) {
+                k--;
+            }
+            else {
+                vector<int> temp = {arr[i], arr[j], arr[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                //skip the duplicates:
+                while (j < k && arr[j] == arr[j - 1]) j++;
+                while (j < k && arr[k] == arr[k + 1]) k--;
+            }
+        }
+    }
+    return ans;
+}
+
+
+int main()
+{
+    vector<int> arr = { -1, 0, 1, 2, -1, -4};
+    int n = arr.size();
+    vector<vector<int>> ans = triplet(n, arr);
+    for (auto it : ans) {
+        cout << "[";
+        for (auto i : it) {
+            cout << i << " ";
+        }
+        cout << "] ";
+    }
+    cout << "\n";
+    return 0;
+}
+
